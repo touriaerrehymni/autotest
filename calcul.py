4926bdd3d99cdde71486820a4af5529e207ff1c6
elmosivio <elmosivio@gmail.com>
2025-04-21 11:37:04 +0100

    actions test auto

---
+++ calcul.py
@@ -0,0 +1,11 @@
+def addition (a,b) :
+    return a + b
+
+def multiplication (a,b) :
+    return a * b
+
+def division (a,b) :
+    if a == b :
+        raise  ValueError("division par zero interdite")
+    return a / b
+
